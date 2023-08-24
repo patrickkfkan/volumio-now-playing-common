ef8441cde6b7dde157daff832936e986c27e3ac6
patrickkfkan <patrickkfkan@gmail.com>
2023-08-24 21:17:07 +0800

    Add `VUMeter` type

---
+++ src/api/vu/index.ts
@@ -0,0 +1,50 @@
+export interface VUMeterBase {
+  type: 'linear' | 'circular';
+  meter: {
+    x: number;
+    y: number;
+  };
+  channels: number;
+  uiRefreshPeriod: number;
+  bgrFilename: string;
+  indicatorFilename: string;
+  screenBgr: string;
+}
+
+export interface VUMeterLinear extends VUMeterBase {
+  type: 'linear';
+  left: {
+    x: number;
+    y: number;
+  };
+  right: {
+    x: number;
+    y: number;
+  };
+  position: {
+    regular: number;
+    overload: number;
+  }
+  stepWidth: {
+    regular: number;
+    overload: number;
+  };
+}
+
+export interface VUMeterCircular extends VUMeterBase {
+  type: 'circular';
+  stepsPerDegree: number;
+  startAngle: number;
+  stopAngle: number;
+  distance: number;
+  leftOrigin: {
+    x: number;
+    y: number;
+  }
+  rightOrigin: {
+    x: number;
+    y: number;
+  }
+}
+
+export type VUMeter = VUMeterLinear | VUMeterCircular;
